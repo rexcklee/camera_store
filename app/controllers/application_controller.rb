98fcb087a39d2559d78ea3bfe8fad836e887965f
rexcklee <rexcklee852@gmail.com>
2024-11-11 23:43:45 -0600

    added footer of contact information and open hours

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :load_store_contact
+
+  private
+  def load_store_contact
+    @store_contacts = StoreContact.all
+  end
 end
