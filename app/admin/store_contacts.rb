207ed3975433eeb4604f70b37a089c527ac99d7b
rexcklee <rexcklee852@gmail.com>
2024-11-14 12:53:15 -0600

    bug fix - getting updated products without showing new created products

--- app/admin/store_contacts.rb
+++ app/admin/store_contacts.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register StoreContact do
   menu priority: 6
   permit_params :location, :phone, :email, :open_mon, :open_tue, :open_wed, :open_thu, :open_fri, :open_sat, :open_sun
-  actions :all
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
     today_date = Date.today
     three_day_before = today_date - 2
     @new_products = Product.where(created_at: (three_day_before.to_date.beginning_of_day)..(today_date.to_date.end_of_day))
-    @updated_products = Product.where(updated_at: (three_day_before.to_date.beginning_of_day)..(today_date.to_date.end_of_day))
+    @updated_products = Product.where(updated_at: (three_day_before.to_date.beginning_of_day)..(today_date.to_date.end_of_day)).and(Product.where("created_at < ?", three_day_before.to_date.beginning_of_day))
   end
 end
