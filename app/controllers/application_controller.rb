bebc367d909cfefd154c1c33f9765aae83461626
rexcklee <rexcklee852@gmail.com>
2024-11-17 01:17:08 -0600

    added basic add product to cart feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,21 @@ class ApplicationController < ActionController::Base
 
   before_action :load_store_contact
 
+  before_action :initialize_session
+
+  helper_method :cart
+
   private
   def load_store_contact
     @main_store_contact = StoreContact.first
+  end
+
+  def initialize_session
+    session[:cart] ||= []
+  end
+
+  def cart
+    product_id = session[:cart]
+    Product.find(product_id)
   end
 end
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,21 @@
 class CartController < ApplicationController
   def create
+    product_id = params[:id].to_i
+    unless session[:cart].include?(product_id)
+      # session[:cart] << { "id": product_id, "qty": 1 }
+      session[:cart] << product_id
+      flash[:notice] = "Item added."
+    end
+    # logger.debug("Session Cart: #{session[:cart]}")
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    product_id = params[:id].to_i
+    if session[:cart].include?(product_id)
+      session[:cart].delete(product_id)
+      flash[:notice] = "Item removed."
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
