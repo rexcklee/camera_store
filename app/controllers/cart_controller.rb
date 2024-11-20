70f9c9a5b9e934dfd10a671cf6af8336b557d708
rexcklee <rexcklee852@gmail.com>
2024-11-19 22:20:31 -0600

    added customer while creating a new order

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -67,7 +67,7 @@ class CartController < ApplicationController
     province = session[:province]
     total_cents = params[:total_cents]
     @order_number = generate_unique_order_number
-    order = Order.create(number: @order_number, address: address, province: province, total_cents: total_cents)
+    order = Order.create(number: @order_number, customer: current_customer, address: address, province: province, total_cents: total_cents)
     cart.each do |product|
       if item = session[:cart].find { |cart_product| cart_product["id"]== product.id }
         OrderProduct.create(order: order, product: product, quantity: item["qty"], unit_price_cents: product.price_cents)
