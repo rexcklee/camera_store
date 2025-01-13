e7a7d9f4d4fca2cdbe86029bbe0acd9a2ff5ad16
rexcklee <rexcklee852@gmail.com>
2025-01-13 15:47:39 -0600

    specify ruby version and added "product coming soon image"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
+  config.hosts << "rexlee.space:3000"
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
